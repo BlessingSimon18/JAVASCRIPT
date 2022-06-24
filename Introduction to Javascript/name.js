12dc9defee40a650a7ee680c911dc0a221bfd10d
BlessingSimon18 <105825808+BlessingSimon18@users.noreply.github.com>
2022-06-24 10:17:37 +0100

    Add files via upload

---
+++ Introduction to Javascript/name.js
@@ -0,0 +1,4 @@
+// Write a javascript code that prints out your name, height, and country on the screen
+console.log("Name: My name is Blessing Simon.")
+console.log("Height: I am 1.57m in height.")
+console.log("Country: I am from Nigeria.")
